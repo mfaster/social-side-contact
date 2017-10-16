1fc3d23f45bbbdfb8c1274aba4560fb262706ffc
Q <mfaster@mfaster.com>
2017-10-16 17:20:25 +0700

    start

---
+++ social-contact.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Social Contact
+ * Plugin URI: https://mfaster.com
+ * Description: This plugin adds social contract on page ,to let customer easy to contact you guys
+ * Version: 1.0.0
+ * Author: Q_Q
+ * Author URI: https://mfaster.com
+ * License: GPL2
+ */
